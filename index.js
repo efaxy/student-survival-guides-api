1cd3ce70af57efe3f5301cc8c74bf6220fb0fd64
yuma <nak071104@gmail.com>
2026-04-07 20:09:38 +0500

    DB connect

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+// Constants
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+
+async function startServer() {
+    try {
+        await mongoose.connect(DB_URL);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+
+    app.listen(PORT, () => {
+        console.log("Server is running on port: " + PORT);
+    });
+}
+
+startServer();
