b118caa7466e10dbaf95e37374535874be9b9aa7
yuma <nak071104@gmail.com>
2026-04-23 14:34:54 +0500

    delete imgUrl from post model

--- models/Post.js
+++ models/Post.js
@@ -5,7 +5,6 @@ const PostSchema = new mongoose.Schema({
     username: {type: String},
     title: {type: String, required: true},
     text: {type: String, required: true},
-    imgUrl: {type: String, default: ''},
     views: {type: Number, default: 0},
     author: {type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     comments: {type: mongoose.Schema.Types.ObjectId, ref: 'Comment'},
